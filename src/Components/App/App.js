17441fe4bb8d4e6306e3fec58778a456253dba07
Latane S <Latanewrites@gmail.com>
2022-12-29 17:45:55 -0500

    initial commit~

---
+++ src/Components/App/App.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import './App.css';
+import Playlist from "../Playlist/Playlist";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Spotify from "../../util/Spotify";
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = { 
+      searchResults: [],
+      playlistName: 'New Playlist',
+      playlistTracks: [] 
+    };
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  addTrack(track) {
+    const tracks = this.state.playlistTracks;
+    const combinedTracks = [];
+    const checkPlaylist = (e) => e.id === track.id;
+
+    if (tracks.some(checkPlaylist) === false) {
+      combinedTracks.push(track);
+    };
+
+    const newPlaylist = tracks.concat(combinedTracks);
+    this.setState({playlistTracks : newPlaylist})
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({playlistTracks : tracks});
+  }
+
+  updatePlaylistName(name){
+    this.setState({ playlistName: name })
+
+  }
+
+  savePlaylist(){
+    const tracks = this.state.playlistTracks;
+    const trackURIs = tracks.map (currentTrack => currentTrack.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+      this.setState({ playlistName: 'New Playlist',
+                      playlistTracks: [] })
+    })
+  }
+
+  search(term){
+    Spotify.search(term).then(searchTerms => {
+      this.setState({searchResults: searchTerms})
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar onSearch={this.search} />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName} 
+                      playlistTracks={this.state.playlistTracks} 
+                      onRemove={this.removeTrack} 
+                      onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist} />
+          </div>
+        </div>
+      </div>
+  )}
+
+  componentDidMount() {
+    window.addEventListener('load', Spotify.search(''));
+  }
+  
+}
+
+
+
+export default App;
